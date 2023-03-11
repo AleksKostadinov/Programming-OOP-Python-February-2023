87fb6c5666b00c9d0e885fc11f11acbc75be3ec1
AleksKostadinov <kostadinov12@gmail.com>
2023-03-11 20:43:14 +0200

    Exercises

---
+++ polymorphism_and_abstraction/image_area.py
@@ -0,0 +1,37 @@
+class ImageArea:
+
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+        self.area = self.get_area()
+
+    def get_area(self):
+        self.area = self.width * self.height
+        return self.area
+
+    def __gt__(self, other):
+        return self.area > other.area
+
+    def __ge__(self, other):
+        return self.area >= other.area
+
+    def __lt__(self, other):
+        return self.area < other.area
+
+    def __le__(self, other):
+        return self.area <= other.area
+
+    def __eq__(self, other):
+        return self.area == other.area
+
+    def __ne__(self, other):
+        return self.area != other.area
+
+
+a1 = ImageArea(7, 10)
+a2 = ImageArea(35, 2)
+a3 = ImageArea(8, 9)
+print(a1 <= a2)
+print(a1 < a3)
+
+
