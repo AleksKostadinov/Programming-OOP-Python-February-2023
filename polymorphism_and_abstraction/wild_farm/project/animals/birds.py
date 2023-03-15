3a4376903ee4039fb3bc5d606179f39e80cf0cbe
AleksKostadinov <kostadinov12@gmail.com>
2023-03-15 15:39:05 +0200

    Exercises

---
+++ polymorphism_and_abstraction/wild_farm/project/animals/birds.py
@@ -0,0 +1,24 @@
+from project.animals.animal import Bird
+from project.food import Food, Meat
+
+
+class Owl(Bird):
+    @staticmethod
+    def make_sound():
+        return 'Hoot Hoot'
+
+    def feed(self, food: Food):
+        if not isinstance(food, Meat):
+            return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
+        self.weight += 0.25 * food.quantity
+        self.food_eaten += food.quantity
+
+
+class Hen(Bird):
+    @staticmethod
+    def make_sound():
+        return 'Cluck'
+
+    def feed(self, food: Food):
+        self.weight += 0.35 * food.quantity
+        self.food_eaten += food.quantity
